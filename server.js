0b2470af1b9d8916058e1577d3489aa0f62518ec
V Suhas <suhas3145@gmail.com>
2023-07-06 04:45:36 +0530

    app sunnung through 5000

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ import dotenv from 'dotenv'
 dotenv.config()
 import 'express-async-errors'
 import morgan from 'morgan'
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+
+
+
 //db and authenticateUser
 import connectDB from './db/connect.js'
 
@@ -22,17 +30,24 @@ import authenticateUser from './middleware/auth.js'
 if(process.env.NODE_ENV!='production'){
     app.use(morgan('dev'))
 }
-app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.json({msg:'Welcome!'})
-})
-app.get('/api/v1',(req,res)=>{
-    res.json({msg:'APi!'})
-})
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.json())
+// app.get('/',(req,res)=>{
+//     res.json({msg:'Welcome!'})
+// })
+// app.get('/api/v1',(req,res)=>{
+//     res.json({msg:'APi!'})
+// })
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+
+app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  });
 
 
 app.use(notFoundMiddleware)
