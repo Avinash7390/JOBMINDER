440b5384d639672d5cc03fa3d3056d8172f3be14
V Suhas <suhas3145@gmail.com>
2023-07-06 03:21:50 +0530

    charts of stats page done

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,15 +1,23 @@
+import React, { useState } from 'react'
 
+import BarChart from './BarChart'
+import AreaChart from './AreaChart'
+import { useAppContext } from '../context/appContext'
+import Wrapper from '../assets/wrappers/ChartsContainer'
 
 const ChartsContainer = () => {
+  const [barChart, setBarChart] = useState(true)
+  const { monthlyApplications: data } = useAppContext()
   return (
-    <div>
-        <h1>charts container</h1>
-    </div>
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
   )
 }
 
 export default ChartsContainer
-
-
-
 
